f201eca871217d54e90f2cb017f0548a57b1ea99
MBriedis <mbriedis@gmail.com>
2018-06-10 17:27:24 +0300

    JSONP translation export, tests

---
+++ src/MessageJsBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Printful\GettextCms;
+
+use Gettext\Generators\Jed;
+
+/**
+ * Class exports JS translations to JSONP callback files in JED translation format
+ */
+class MessageJsBuilder
+{
+    /** @var MessageStorage */
+    private $storage;
+
+    public function __construct(MessageStorage $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    /**
+     * Export one or multiple domains in a single JS file with JSONP callback that will load JED format translations
+     *
+     * @param string $locale
+     * @param string[] $domains
+     * @param string $jsonpCallback Function name, that will be called with domain translations
+     * @return string
+     */
+    public function exportJsonp(string $locale, array $domains, string $jsonpCallback)
+    {
+        $js = '';
+
+        foreach ($domains as $domain) {
+            $translations = $this->storage->getEnabledTranslatedJs($locale, $domain);
+
+            $jed = Jed::toString($translations);
+
+            $js .= $jsonpCallback . '(' . $jed . ");\n";
+        }
+
+        return $js;
+    }
+}
---
+++ tests/TestCases/JsBuilderTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Printful\GettextCms\Tests\TestCases;
+
+use Gettext\Translation;
+use Printful\GettextCms\MessageJsBuilder;
+use Printful\GettextCms\MessageStorage;
+use Printful\GettextCms\Tests\Stubs\MessageRepositoryStub;
+use Printful\GettextCms\Tests\TestCase;
+
+class JsBuilderTest extends TestCase
+{
+    private $locale;
+
+    /** @var MessageStorage */
+    private $storage;
+
+    /** @var MessageJsBuilder */
+    private $builder;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->storage = new MessageStorage(new MessageRepositoryStub);
+        $this->builder = new MessageJSBuilder($this->storage);
+    }
+
+    public function testExportJsonp()
+    {
+        $this->locale = 'en_US';
+
+        $this->add('D1', true, 'D1-O1', 'D1-T1');
+        $this->add('D1', true, 'D1-O2', 'D1-T2');
+        $this->add('D1', false, 'non-js', 'non-js'); // Non-js translation
+        $this->add('D2', true, 'D2-O1', 'D2-T2');
+
+        $domains = ['D1', 'D2'];
+
+        $js = $this->builder->exportJsonp($this->locale, $domains, 'loaded');
+
+        self::assertNotContains('non-js', $js, 'Non-js translation is not included');
+
+        foreach ($domains as $domain) {
+            $ts = $this->storage->getEnabledTranslatedJs($this->locale, $domain);
+
+            self::assertTrue(count($ts) > 0, 'Domain ' . $domain . ' has translations');
+
+            foreach ($ts as $t) {
+                /** @var Translation $t */
+                self::assertContains($t->getOriginal(), $js, $t->getOriginal() . ' is within JS');
+                self::assertContains($t->getTranslation(), $js, $t->getTranslation() . ' translation is within JS');
+            }
+        }
+    }
+
+    private function add(string $domain, bool $isJs, string $original, string $translation, string $context = ''): self
+    {
+        $translation = (new Translation($context, $original))->setTranslation($translation);
+
+        if ($isJs) {
+            $translation->addReference('file.js', 1);
+        }
+
+        $this->storage->saveSingleTranslation(
+            $this->locale,
+            $domain,
+            $translation
+        );
+
+        return $this;
+    }
+}
